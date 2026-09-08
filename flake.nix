b367633c314b6109f4ca695b01eeca74066c7b56
Graham <gleachkr@gmail.com>
2026-09-08 18:16:34 -0400

    Bump some flake dependencies

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     naersk = {
-      url = "github:nmattia/naersk";
+      url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     fenix = {
